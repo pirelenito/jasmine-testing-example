7dceb4bbe72710aca9efb29b3c1fd7daf4affcc4
Paulo Ragonha <paulo@ragonha.me>
2013-04-13 16:50:41 -0300

    book friendlier Stock code

--- src/Stock.js
+++ src/Stock.js
@@ -5,25 +5,31 @@
     this.sharePrice = params.sharePrice;
   };
 
-  Stock.prototype = {
-    fetch: function (params) {
-      var that = this;
-      var success = params.success || function () {}
+  Stock.prototype.fetch = function(params) {
+    var that = this;
+    var params = params || {};
+    var success = params.success || function () {}
 
-      $.ajax({
-        url: 'http://query.yahooapis.com/v1/public/yql',
-        dataType: 'jsonp',
-        data: {
-          format: 'json',
-          env: 'http://datatables.org/alltables.env',
-          q: 'select Ask from yahoo.finance.quotes where symbol=\''+that.symbol+'\''
-        },
-        success: function (data) {
-          that.sharePrice = parseFloat(data.query.results.quote.Ask);
-          success(that);
-        }
-      });
-    }
+    $.ajax({
+      url: 'http://query.yahooapis.com/v1/public/yql',
+      dataType: 'jsonp',
+      data: {
+        format: 'json',
+        env: 'http://datatables.org/alltables.env',
+        q: queryStringForSymbol(that.symbol)
+      },
+      success: function (data) {
+        console.log(data)
+        that.sharePrice = parseFloat(data.query.results.quote.Ask);
+        success(that);
+      }
+    });
+  };
+
+  function queryStringForSymbol (symbol) {
+    return 'select Ask ' +
+           'from yahoo.finance.quotes ' +
+           'where symbol=\''+symbol+'\'';
   }
 
   global.Stock = Stock;
