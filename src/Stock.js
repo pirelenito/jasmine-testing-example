c4a1dedebe36776d2aa53a9bb62add909966dac8
Paulo Ragonha <paulo@ragonha.me>
2013-04-13 16:19:27 -0300

    simpler Stock fetch, easier to put on the book

--- src/Stock.js
+++ src/Stock.js
@@ -10,42 +10,20 @@
       var that = this;
       var success = params.success || function () {}
 
-      queryYahooFinance({
-        symbol: that.symbol,
-        success: function (stock) {
-          that.sharePrice = stock.sharePrice;
-          that.change = stock.change;
+      $.ajax({
+        url: 'http://query.yahooapis.com/v1/public/yql',
+        dataType: 'jsonp',
+        data: {
+          format: 'json',
+          env: 'http://datatables.org/alltables.env',
+          q: 'select Ask, Change from yahoo.finance.quotes where symbol in (\''+that.symbol+'\')'
+        },
+        success: function (data) {
+          that.sharePrice = parseFloat(data.query.results.quote.Ask, 10);
+          that.change = parseFloat(data.query.results.quote.Change, 10);
           success(that);
         }
       });
-    }
-  }
-
-  function queryYahooFinance(params) {
-    var that = this;
-    var symbol = params.symbol;
-    var success = params.success;
-    var query = 'select Ask, Change from yahoo.finance.quotes where symbol in (\''+symbol+'\')';
-
-    $.ajax({
-      url: 'http://query.yahooapis.com/v1/public/yql',
-      dataType: 'jsonp',
-      data: {
-        format: 'json',
-        env: 'http://datatables.org/alltables.env',
-        q: query
-      },
-      success: function (data) {
-        console.log(data)
-        success(parseStockData(data));
-      }
-    });
-  }
-
-  function parseStockData (data) {
-    return {
-      sharePrice: parseFloat(data.query.results.quote.Ask, 10),
-      change: parseFloat(data.query.results.quote.Change, 10)
     }
   }
 
