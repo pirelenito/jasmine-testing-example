77fdbda055850287c09ae2b06334d3e6f838afe0
Paulo Ragonha <paulo@ragonha.me>
2013-04-16 16:03:37 -0300

    simple node server to get stock quotes from yahoo

---
+++ server.js
@@ -0,0 +1,26 @@
+var express = require('express');
+var request = require('request');
+var app = express();
+
+app.get('/stocks/:symbol', function (req, res) {
+  var symbol = req.params.symbol;
+  var query = 'select Ask ' +
+                'from yahoo.finance.quotes ' +
+                'where symbol=\'' + symbol + '\'';
+
+  res.setHeader('Content-Type', 'application/json');
+
+  request({
+    uri: 'http://query.yahooapis.com/v1/public/yql',
+    method: 'GET',
+    qs: {
+      format: 'json',
+      env: 'http://datatables.org/alltables.env',
+      q: query
+    }
+  }, function(error, response, body) {
+    res.send(response.body)
+  })
+})
+
+app.listen(8000);
