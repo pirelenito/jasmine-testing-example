a1e1657cba29a3001c57b2c5b22c364f27ea3753
Paulo Ragonha <paulo@ragonha.me>
2013-04-25 14:25:38 -0300

    merges all StockSpecs into a single file, leaving only one implementation of the fetch spec.

--- spec/StockSpec.js
+++ spec/StockSpec.js
@@ -12,4 +12,35 @@ describe("Stock", function() {
   it("should have a share price", function() {
     expect(stock.sharePrice).toEqual(originalSharePrice);
   });
+
+  describe("when fetched", function() {
+    var xhr;
+
+    beforeEach(function() {
+      xhr = sinon.fakeServer.create();
+      xhr.respondWith(
+        'GET',
+        /\/stocks\/(.+)/,
+        function (request, stockSymbol) {
+          request.respond(
+            200,
+            { "Content-Type": "application/json" },
+            '{ "sharePrice": 20.13 }'
+          )
+        }
+      );
+
+      stock.fetch();
+
+      xhr.respond();
+    });
+
+    afterEach(function() {
+      xhr.restore();
+    });
+
+    it("should update its share price", function() {
+      expect(stock.sharePrice).toEqual(20.13);
+    });
+  });
 });
