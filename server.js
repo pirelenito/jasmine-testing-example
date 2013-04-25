fc5194a4c4f8bdabae772465d9240b57d09f7edc
Paulo Ragonha <paulo@ragonha.me>
2013-04-25 14:17:53 -0300

    simpler server (only a mock)

--- server.js
+++ server.js
@@ -1,36 +1,11 @@
 var express = require('express');
-var request = require('request');
-
 var app = express();
 
-app.get('/stocks/:symbol', function (request, response) {
-  fetchYahooFinance(request.params.symbol, function (data) {
-    response.setHeader('Content-Type', 'application/json');
-    response.send({
-      sharePrice: data.query.results.quote.Ask
-    });
-  });
+app.get('/stocks/:symbol', function (req, res) {
+  res.setHeader('Content-Type', 'application/json');
+  res.send({ sharePrice: 20.18 });
 });
 
-function fetchYahooFinance (symbol, callback) {
-  var query = 'select Ask ' +
-              'from yahoo.finance.quotes ' +
-              'where symbol=\'' + symbol + '\'';
-
-  request({
-    uri: 'http://query.yahooapis.com/v1/public/yql',
-    method: 'GET',
-    qs: {
-      format: 'json',
-      env: 'http://datatables.org/alltables.env',
-      q: query
-    }
-  }, function(_, _, body) {
-    callback(JSON.parse(body));
-  });
-}
-
-// Static server to run the specs
 app.use(express.static(__dirname));
 
 app.listen(8000);
