19167ed7d6c823ecfd2fbc4e77454fe4ca5b3364
Paulo Ragonha <paulo@ragonha.me>
2013-04-28 15:34:40 -0300

    Stock custom properties with get and events preventing outside set

--- src/Investment.js
+++ src/Investment.js
@@ -2,22 +2,38 @@
 
   var Investment = Backbone.Model.extend({
     initialize: function () {
-      this.set('cost', this.get('shares') * this.get('sharePrice'));
+      this.on('change:sharePrice', updateCost, this);
+      this.on('change:shares', updateCost, this);
+
+      this.on('change:roi', updateIsGood, this);
 
       this.on('change:sharePrice', updateROI, this);
-      this.on('change:roi', updateIsGood, this);
       this.listenTo(this.get('stock'), 'change:sharePrice', updateROI, this);
+
+      this.on('change:cost change:isGood change:roi', preventSetter, this);
+
+      updateROI.call(this);
+      updateIsGood.call(this);
+      updateCost.call(this);
     }
   });
 
   function updateROI () {
     var sharePrice = this.get('sharePrice');
     var stockSharePrice = this.get('stock').get('sharePrice');
-    this.set('roi', (stockSharePrice - sharePrice) / sharePrice);
+    this.set('roi', (stockSharePrice - sharePrice) / sharePrice, { inner: true });
   }
 
   function updateIsGood () {
-    this.set('isGood', this.get('roi') > 0);
+    this.set('isGood', this.get('roi') > 0, { inner: true });
+  }
+
+  function updateCost () {
+    this.set('cost', this.get('shares') * this.get('sharePrice'), { inner: true });
+  }
+
+  function preventSetter (_, _, options) {
+    if (!options.inner) { throw 'nOOOOO' };
   }
 
   global.Investment = Investment;
