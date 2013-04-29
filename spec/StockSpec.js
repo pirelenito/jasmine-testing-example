68e35ec4e21f719b2728477ffe300879e628b086
Paulo Ragonha <paulo@ragonha.me>
2013-04-29 16:26:38 -0300

    nice and clean Stock spec

--- spec/StockSpec.js
+++ spec/StockSpec.js
@@ -1,50 +1,23 @@
 describe("Stock", function() {
   var stock;
-  var originalSharePrice = 0;
 
   beforeEach(function() {
     stock = new Stock({
-      symbol: 'YHOO',
-      sharePrice: originalSharePrice
+      symbol: 'YHOO'
     })
   });
 
-  it("should have a share price", function() {
-    expect(stock.get('sharePrice')).toEqual(originalSharePrice);
+  it("should be a Backbone.Model", function() {
+    expect(stock).toEqual(jasmine.any(Backbone.Model));
   });
 
-  describe("when fetched", function() {
-    var xhr;
-
-    beforeEach(function() {
-      xhr = sinon.fakeServer.create();
-      xhr.respondWith(
-        'GET',
-        /\/stocks\/(.+)/,
-        function (request, stockSymbol) {
-          request.respond(
-            200,
-            { "Content-Type": "application/json" },
-            '{ "sharePrice": 20.13 }'
-          )
-        }
-      );
-
-      stock.fetch();
-
-      xhr.respond();
+  describe("sync", function() {
+    it("should use the stock symbol as the id when making requests", function() {
+      expect(stock.idAttribute).toEqual('symbol');
     });
 
-    afterEach(function() {
-      xhr.restore();
-    });
-
-    it("should have made a request with the stock symbol", function() {
-      expect(xhr.requests[0].url).toEqual('/stocks/YHOO');
-    });
-
-    it("should update its share price", function() {
-      expect(stock.get('sharePrice')).toEqual(20.13);
+    it("should make requests to the /stocks url", function() {
+      expect(stock.urlRoot).toEqual('/stocks');
     });
   });
 });
