fa12d4c8fd4fb451737663b95f3f31807bfc12a5
Paulo Ragonha <paulo@ragonha.me>
2013-05-07 20:56:05 -0300

    removes old spec

--- spec/InvestmentsRouterSpec.js
+++ spec/InvestmentsRouterSpec.js
@@ -1,40 +1,36 @@
 describe("InvestmentsRouter", function() {
-  var router, callback;
+  var router, observer;
 
   beforeEach(function() {
     router = new InvestmentsRouter();
-    callback = sinon.spy();
+    observer = sinon.spy();
+  });
+
+  it("should be a Backbone Router", function() {
+    expect(router).toEqual(jasmine.any(Backbone.Router));
   });
 
   it("should route '/investments/good'", function() {
-    router.on('route:goodInvestments', callback);
+    router.on('route:goodInvestments', observer);
 
     Backbone.history.loadUrl('/investments/good')
 
-    expect(callback).toHaveBeenCalled();
+    expect(observer).toHaveBeenCalled();
   });
 
   it("should route '/investments/bad'", function() {
-    router.on('route:badInvestments', callback);
+    router.on('route:badInvestments', observer);
 
     Backbone.history.loadUrl('/investments/bad')
 
-    expect(callback).toHaveBeenCalled();
+    expect(observer).toHaveBeenCalled();
   });
 
   it("should route '/investments/all'", function() {
-    router.on('route:allInvestments', callback);
+    router.on('route:allInvestments', observer);
 
     Backbone.history.loadUrl('/investments/all')
 
-    expect(callback).toHaveBeenCalled();
-  });
-
-  it("should route '/investments/:id'", function() {
-    router.on('route:investment', callback);
-
-    Backbone.history.loadUrl('/investments/42')
-
-    expect(callback).toHaveBeenCalled('42');
+    expect(observer).toHaveBeenCalled();
   });
 });
