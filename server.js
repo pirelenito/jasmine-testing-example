517201db8bf508d3dfddbb0fc44021c5c509fa7e
Paulo Ragonha <paulo@ragonha.me>
2013-05-07 22:46:34 -0300

    server implementation of the stocks route

--- server.js
+++ server.js
@@ -1,6 +1,17 @@
 var express = require('express');
 var app = express();
 
+app.get('/stocks', function (req, res) {
+  var ids = req.query.ids || [];
+
+  var stocks = ids.map(function (value) {
+    return { symbol: value, sharePrice: 20.18 };
+  }, this)
+
+  res.setHeader('Content-Type', 'application/json');
+  res.send(stocks);
+});
+
 app.get('/stocks/:symbol', function (req, res) {
   res.setHeader('Content-Type', 'application/json');
   res.send({ sharePrice: 20.18 });
