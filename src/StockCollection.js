dc1ebe4efbd805ae54342e5f55cadc27fdfe2f73
Paulo Ragonha <paulo@ragonha.me>
2013-05-07 22:48:42 -0300

    refactor Stock Collection

--- src/StockCollection.js
+++ src/StockCollection.js
@@ -3,21 +3,20 @@
   var StockCollection = Backbone.Collection.extend({
     model: Stock,
     url: function () {
-      return "/stocks" + queryString(modelIds.call(this));
+      return "/stocks" + idsQueryString.call(this);
     }
   });
 
   function modelIds () {
-    return this.map(function (model) {
-      return model.id;
-    })
+    return this.map(function (model) { return model.id; });
   }
 
-  function queryString (ids) {
+  function idsQueryString () {
+    var ids = modelIds.call(this);
+
     if (ids.length === 0) { return ''; }
-    return '?' + $.param({
-      ids: ids
-    });
+
+    return '?' + $.param({ ids: ids });
   }
 
   global.StockCollection = StockCollection;
